cb7e06112c164a719b111b760dcaf29d2f415e41
Graydon Armstrong <seeds77222@hotmail.com>
2013-06-06 01:22:33 -0400

    Initial Commit for the start of the assignment

---
+++ assignment2v1.py
@@ -0,0 +1,15 @@
+# Name: Graydon Armstrong
+# Date: June 5th, 2013
+# Source File: assignment2v1.py
+# Last Modified By: Graydon Armstrong
+# Date Last Modified: June 5th, 2013
+# Program description: A slot machine game where the player can bet money and win
+# Revision History: Version 1 is the text version of the app
+
+
+
+def main():
+    temp = "Hello World"
+    
+# run the main class        
+if __name__ == "__main__": main()
