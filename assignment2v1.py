0aed6e3b16dca12ae1888123a0d96fba2de7eaef
Graydon Armstrong <seeds77222@hotmail.com>
2013-06-07 00:28:12 -0400

    Added a main pygame game loop

--- assignment2v1.py
+++ assignment2v1.py
@@ -4,12 +4,50 @@
 # Last Modified By: Graydon Armstrong
 # Date Last Modified: June 5th, 2013
 # Program description: A slot machine game where the player can bet money and win
-# Revision History: Version 1 is the text version of the app
+# Revision History: Version 1 is drawing a temporary GUI for the slot machine to try out pygame
+
+#I - Import and initialize
+import pygame
+pygame.init()
+
+#D - Display configuration
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Hello, world!")
+
+#E - Entities (just background for now)
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill((0, 0, 255))
+
+#A - Action (broken into ALTER steps)
+
+    #A - Assign values to key variables
+clock = pygame.time.Clock()
+keepGoing = True
+
+    #L - Set up main loop
+while keepGoing:
+
+    #T - Timer to set frame rate
+    clock.tick(30)
+
+    #E - Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            keepGoing = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print pygame.mouse.get_pos()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            print pygame.mouse.get_pos()
+
+    #R - Refresh display
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
 
 
-
+"""
 def main():
     temp = "Hello World"
     
 # run the main class        
-if __name__ == "__main__": main()
+if __name__ == "__main__": main()""" 
