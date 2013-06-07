794fd4010fde2cbd95ed6b1dac1bacd158a379ec
Graydon Armstrong <seeds77222@hotmail.com>
2013-06-07 00:37:10 -0400

    Placed a box on screen that you can move position by clicking

--- assignment2v1.py
+++ assignment2v1.py
@@ -10,16 +10,21 @@
 import pygame
 pygame.init()
 
-
 def main():
     #D - Display configuration
     screen = pygame.display.set_mode((640, 480))
     pygame.display.set_caption("Hello, world!")
+    x_pos = 200
+    y_pos = 200
     
     #E - Entities (just background for now)
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((0, 0, 255))
+    
+    box = pygame.Surface((100,100))
+    box = box.convert()
+    box.fill((50,50,50))
     
     #A - Action (broken into ALTER steps)
     
@@ -39,13 +44,15 @@ def main():
                 keepGoing = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 print pygame.mouse.get_pos()
-            elif event.type == pygame.MOUSEBUTTONUP:
-                print pygame.mouse.get_pos()
+                coordinates = list(pygame.mouse.get_pos())
+                x_pos = coordinates[0]
+                y_pos = coordinates[1]
     
         #R - Refresh display
         screen.blit(background, (0, 0))
+        screen.blit(box,(x_pos,y_pos))
         pygame.display.flip()
 
     
 # run the main class        
-if __name__ == "__main__": main()""" 
+if __name__ == "__main__": main()
